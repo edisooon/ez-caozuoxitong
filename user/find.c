3086333c6fcdf0ecd1ab54433d6d84836a853413
Edison Zhang <edisonzhang12789@gmail.com>
2025-11-05 23:35:13 -0800

    implemented exec with fork and exec syscalls

--- user/find.c
+++ user/find.c
@@ -1,11 +1,12 @@
 #include "kernel/types.h"
+#include "kernel/param.h"
 #include "kernel/stat.h"
 #include "user/user.h"
 #include "kernel/fs.h"
 #include "kernel/fcntl.h"
 
 void
-find(char *dir, char *filename)
+find(char *dir, char *filename, int argc, char *argv[])
 {
     char buf[512], *p;
     int fd;
@@ -46,12 +47,33 @@ find(char *dir, char *filename)
         switch(st.type) {
             case T_DEVICE:
             case T_FILE:
-                if(strcmp(de.name, filename) == 0)
-                    printf("%s\n", buf); 
+                if(strcmp(de.name, filename) == 0) {
+                    if(argc>=5 && strcmp(argv[3], "-exec")==0 ) {
+                        char *new_argv[MAXARG];
+                        int new_argc = argc;
+                        int pid;
+                        memmove(new_argv, argv+4, sizeof(char**) * (new_argc-=4));
+                        new_argv[new_argc++] = buf;
+
+                        pid = fork();
+                        if(pid > 0) {
+                            wait(0);
+                        }else if(pid == 0) {
+                            exec(new_argv[0], new_argv);
+                            fprintf(2, "find: exec error\n");
+                            exit(1);
+                        }else{
+                            fprintf(2, "find: fork error\n");
+                            exit(1);
+                        }
+                    } else {
+                        printf("%s\n", buf); 
+                    }
+                }
                 break;
             case T_DIR:
                 if(strcmp(de.name, ".") != 0 && (strcmp(de.name, "..") != 0)) 
-                    find(buf, filename);
+                    find(buf, filename, argc, argv);
                 break;
         }
         
@@ -70,6 +92,6 @@ main(int argc, char *argv[])
     char* dir = argv[1];
     char* filename = argv[2];
 
-    find(dir, filename);
+    find(dir, filename, argc, argv);
     exit(0);
 }
